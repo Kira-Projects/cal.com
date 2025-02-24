53739d3c21d788b570720a009ed3a59d3f114c6b
leandro-codee <leandrocode2785@gmail.com>
2025-02-24 09:15:53 -0300

    change schema public to agendix

--- packages/lib/constants.ts
+++ packages/lib/constants.ts
@@ -16,7 +16,7 @@ export const WEBAPP_URL =
   RAILWAY_STATIC_URL ||
   HEROKU_URL ||
   RENDER_URL ||
-  "https://app.agendix.site";
+  "http://localhost:3000";
 
 // OAuth needs to have HTTPS(which is not generally setup locally) and a valid tld(*.local isn't a valid tld)
 // So for development purpose, we would stick to localhost only
